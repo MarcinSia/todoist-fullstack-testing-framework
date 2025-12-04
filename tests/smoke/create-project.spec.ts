2d5398f5ea98a612f68f1718d597f123371bdd1b
Marcin Siatkowski <marcin.siatkowski@o2.pl>
2025-12-04 11:26:40 +0100

    M05L01 - feat(api): implement getProjectIdByName API step and model

--- tests/smoke/create-project.spec.ts
+++ tests/smoke/create-project.spec.ts
@@ -1,9 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { expect, test } from '../../src/fixtures/po.fixture';
 import { CreateProjectModel } from '../../src/models/create-project.model';
-import { API_URL, PROJECTS_ENDPOINT } from '../../src/api/config/api.config';
-import { getRequest } from '../../src/api/requests/get.request';
-import { TODOIST_AUTH_HEADERS } from '../../src/api/requests/todoist.auth.headers';
+import { getProjectIdByNameAPISteps } from '../../src/api/steps/projects/read.project.api.step';
 
 test('should create a new project', { tag: ['@smoke', '@smoke001'] }, async ({ homePage }) => {
   // Arrange
@@ -17,7 +15,9 @@ test('should create a new project', { tag: ['@smoke', '@smoke001'] }, async ({ h
   await homePage.leftPanel.addNewProject(project);
 
   // API check
-  await getRequest(`${API_URL}/${PROJECTS_ENDPOINT}`, TODOIST_AUTH_HEADERS);
+  const projectId =await getProjectIdByNameAPISteps(project.name);
+  expect(projectId).not.toBeNull();
+
 
   // Assert
   await expect(homePage.leftPanel.getProjectByName(project.name)).toBeVisible();
